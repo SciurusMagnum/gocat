880a95fe1977d177693d2e97149fb37425de4b95
Tomer Schwartz <tomeron12@gmail.com>
2018-04-18 10:35:28 +0300

    started using channels

--- gocat.go
+++ gocat.go
@@ -6,8 +6,27 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 )
 
+func readBuffer(reader *bufio.Reader, channel chan string) {
+	for {
+		var text []byte
+		var chanString string
+		reader.Read(text)
+		chanString = strings.Trim(string(text), "\n	")
+		if chanString != "" {
+			channel <- chanString
+		}
+
+	}
+	// fmt.Fprintf(conn, text)
+}
+
+// if err != nil {
+// 				println("read from server failed:", err.Error())
+// 				os.Exit(1)
+// 			}
 func main() {
 	// strEcho := "Hello\n"
 	servAddr := "localhost:6666"
@@ -24,28 +43,39 @@ func main() {
 	}
 	// println("write to server = ", strEcho)
 	ioReader := bufio.NewReader(os.Stdin)
+	inChannel := make(chan string)
 	// send to socket
 	// listen for reply
 	connReader := bufio.NewReader(conn)
-	for {
-		text, _ := ioReader.ReadString('\n')
-		fmt.Fprintf(conn, text)
-		// _, err = conn.Write([]byte(strEcho))
-		if err != nil {
-			println("Write to server failed:", err.Error())
-			os.Exit(1)
-		}
-		// reply := make([]byte, 1024)
-		reply, err := connReader.ReadString('\n')
-		reply = strings.Trim(reply, "\n	")
-		// _, err = conn.Read(reply)
-		if err != nil {
-			println("read from server failed:", err.Error())
-			os.Exit(1)
-		}
+	connChannel := make(chan string)
+	go readBuffer(connReader, connChannel)
+	go readBuffer(ioReader, inChannel)
 
-		println(string(reply))
+	for {
+		// var inText, connText string
+		// 	text, _ := ioReader.ReadString('\n')
+		// 	fmt.Fprintf(conn, text)
+		// reply := make([]byte, 1024)
+		select {
+		case <-connChannel:
+			println(<-connChannel)
+		case <-inChannel:
+			fmt.Fprintf(conn, <-inChannel)
+		default:
+			time.Sleep(50 * time.Millisecond)
+
+		}
 
 	}
+	// reply, err := connReader.ReadString('\n')
+	// reply = strings.Trim(reply, "\n	")
+	// // _, err = conn.Read(reply)
+	// if err != nil {
+	// 	println("read from server failed:", err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// println(string(reply))
+
 	// conn.Close()
 }
