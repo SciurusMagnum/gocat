02cf5c5c20f9730c593e2f1f4ece84c4702ff34a
Tomer Schwartz <tomeron12@gmail.com>
2018-04-19 20:17:56 +0300

    basic tcp read write works

--- gocat.go
+++ gocat.go
@@ -6,30 +6,42 @@ import (
 	"net"
 	"os"
 	"strings"
-	"time"
 )
 
 func readBuffer(reader *bufio.Reader, channel chan string) {
 	for {
-		var text []byte
-		var chanString string
-		reader.Read(text)
-		chanString = strings.Trim(string(text), "\n	")
-		if chanString != "" {
-			channel <- chanString
+		chanString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Printf("error reading from reader")
+			close(channel)
+			os.Exit(1)
 		}
+		chanString = strings.Trim(chanString, "\n")
+		channel <- chanString
 
 	}
-	// fmt.Fprintf(conn, text)
 }
 
-// if err != nil {
-// 				println("read from server failed:", err.Error())
-// 				os.Exit(1)
-// 			}
-func main() {
-	// strEcho := "Hello\n"
-	servAddr := "localhost:6666"
+func printChannelData(channel chan string) {
+	for data := range channel {
+		println(data)
+	}
+}
+func sendChannelData(channel chan string, conn *net.TCPConn) {
+	writer := bufio.NewWriter(conn)
+	for data := range channel {
+		_, err := writer.WriteString(data + "\n")
+		if err == nil {
+			err = writer.Flush()
+		} else {
+			println("connection closed")
+			break
+		}
+	}
+}
+
+func initializeConnection(servAddr string) *net.TCPConn {
+
 	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
 	if err != nil {
 		println("ResolveTCPAddr failed:", err.Error())
@@ -41,41 +53,21 @@ func main() {
 		println("Dial failed :", err.Error())
 		os.Exit(1)
 	}
-	// println("write to server = ", strEcho)
+	return conn
+}
+
+func main() {
+	servAddr := "localhost:6666"
+	conn := initializeConnection(servAddr)
+	defer conn.Close()
 	ioReader := bufio.NewReader(os.Stdin)
 	inChannel := make(chan string)
-	// send to socket
-	// listen for reply
+
 	connReader := bufio.NewReader(conn)
 	connChannel := make(chan string)
 	go readBuffer(connReader, connChannel)
 	go readBuffer(ioReader, inChannel)
+	go printChannelData(connChannel)
 
-	for {
-		// var inText, connText string
-		// 	text, _ := ioReader.ReadString('\n')
-		// 	fmt.Fprintf(conn, text)
-		// reply := make([]byte, 1024)
-		select {
-		case <-connChannel:
-			println(<-connChannel)
-		case <-inChannel:
-			fmt.Fprintf(conn, <-inChannel)
-		default:
-			time.Sleep(50 * time.Millisecond)
-
-		}
-
-	}
-	// reply, err := connReader.ReadString('\n')
-	// reply = strings.Trim(reply, "\n	")
-	// // _, err = conn.Read(reply)
-	// if err != nil {
-	// 	println("read from server failed:", err.Error())
-	// 	os.Exit(1)
-	// }
-
-	// println(string(reply))
-
-	// conn.Close()
+	sendChannelData(inChannel, conn)
 }
